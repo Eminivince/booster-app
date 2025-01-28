1fa1560d4d8a11c88592b994d806342ca7a59011
eminivince <communicatti@gmail.com>
2025-01-28 14:17:03 +0100

    added remote api url

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,7 @@
 // frontend/src/api/login.js
 import axios from "axios";
 
-const API_BASE = "http://localhost:5080/api";
+const API_BASE = "https://bknd-node-deploy-d242c366d3a5.herokuapp.com/api";
 // const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5080/api";
 /**
  * POST /api/login
